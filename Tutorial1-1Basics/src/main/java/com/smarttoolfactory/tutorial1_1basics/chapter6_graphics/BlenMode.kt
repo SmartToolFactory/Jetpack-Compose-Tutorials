4f6f081e9a0c1c3008fcf6c892f53baa65dcf6d7
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-02-06 22:55:54 +0300

    Add clip image with blend mode sample

---
+++ Tutorial1-1Basics/src/main/java/com/smarttoolfactory/tutorial1_1basics/chapter6_graphics/BlenMode.kt
@@ -0,0 +1,100 @@
+package com.smarttoolfactory.tutorial1_1basics.chapter6_graphics
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.RadioButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BlendModeSelection(
+    modifier: Modifier = Modifier,
+    selectedIndex: Int,
+    onBlendModeSelected: (Int, BlendMode) -> Unit
+) {
+    val radioOptions = blendModes.keys.toList()
+
+
+    val (selectedOption: String, onOptionSelected: (String) -> Unit) = remember {
+        mutableStateOf(
+            radioOptions[selectedIndex]
+        )
+    }
+// Note that Modifier.selectableGroup() is essential to ensure correct accessibility behavior
+    Column(
+        Modifier
+            .selectableGroup()
+            .then(modifier)
+    ) {
+        radioOptions.forEachIndexed { index, text ->
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .selectable(
+                        selected = (text == selectedOption),
+                        onClick = {
+                            onOptionSelected(text)
+                            onBlendModeSelected(index, blendModes[text]!!)
+                        },
+                        role = Role.RadioButton
+                    )
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                RadioButton(
+                    selected = (text == selectedOption),
+                    onClick = null
+                )
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.body1.merge(),
+                    modifier = Modifier.padding(start = 2.dp)
+                )
+            }
+        }
+    }
+}
+
+val blendModes = linkedMapOf(
+    "Clear" to BlendMode.Clear,
+    "Src" to BlendMode.Src,
+    "Dst" to BlendMode.Dst,
+    "SrcOver" to BlendMode.SrcOver,
+    "DstOver" to BlendMode.DstOver,
+    "SrcIn" to BlendMode.SrcIn,
+    "DstIn" to BlendMode.DstIn,
+    "SrcOut" to BlendMode.SrcOut,
+    "DstOut" to BlendMode.DstOut,
+    "SrcAtop" to BlendMode.SrcAtop,
+    "DstAtop" to BlendMode.DstAtop,
+    "Xor" to BlendMode.Xor,
+    "Plus" to BlendMode.Plus,
+    "Modulate" to BlendMode.Modulate,
+    "Screen" to BlendMode.Screen,
+    "Overlay" to BlendMode.Overlay,
+    "Darken" to BlendMode.Darken,
+    "Lighten" to BlendMode.Lighten,
+    "ColorDodge" to BlendMode.ColorDodge,
+    "ColorBurn" to BlendMode.ColorBurn,
+    "HardLight" to BlendMode.Hardlight,
+    "SoftLight" to BlendMode.Softlight,
+    "Difference" to BlendMode.Difference,
+    "Exclusion" to BlendMode.Exclusion,
+    "Multiply" to BlendMode.Multiply,
+    "Hue" to BlendMode.Hue,
+    "Saturation" to BlendMode.Saturation,
+    "Color" to BlendMode.Color,
+    "Luminosity" to BlendMode.Luminosity
+)
--- Tutorial1-1Basics/src/main/java/com/smarttoolfactory/tutorial1_1basics/chapter6_graphics/Tutorial6_2BlendMode.kt
+++ Tutorial1-1Basics/src/main/java/com/smarttoolfactory/tutorial1_1basics/chapter6_graphics/Tutorial6_2BlendMode.kt
@@ -4,27 +4,31 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.selection.selectable
-import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
-import androidx.compose.material.RadioButton
+import androidx.compose.material.Slider
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.nativeCanvas
-import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.smarttoolfactory.tutorial1_1basics.R
 import com.smarttoolfactory.tutorial1_1basics.chapter2_material_widgets.CustomDialogWithResultExample
+import com.smarttoolfactory.tutorial1_1basics.ui.components.TutorialText2
+import kotlin.math.roundToInt
 
+/*
+    For more info about Blend or Porter-Duff modes refer official link below
+    https://developer.android.com/reference/android/graphics/PorterDuff.Mode?hl=eu
+ */
 @Composable
 fun Tutorial6_2Screen() {
     TutorialContent()
@@ -46,8 +50,18 @@ private fun TutorialContent() {
 @Composable
 private fun BlendModeExample() {
 
+    TutorialText2(text = "Draw Shapes with Blend Mode")
+    DrawShapeBlendMode()
+    TutorialText2(text = "Draw Images with Blend Mode")
+    DrawImageBlendMode()
+    TutorialText2(text = "Clip Image with Blend Mode")
+    ClipImageWithBlendMode()
+}
+
+@Composable
+private fun DrawShapeBlendMode() {
     var selectedIndex by remember { mutableStateOf(3) }
-    var blendMode: BlendMode by remember { mutableStateOf(BlendMode.DstOver) }
+    var blendMode: BlendMode by remember { mutableStateOf(BlendMode.SrcOver) }
 
     var srcColor by remember { mutableStateOf(Color(0xff29B6F6)) }
     var dstColor by remember { mutableStateOf(Color(0xffEC407A)) }
@@ -65,27 +79,23 @@ private fun BlendModeExample() {
         val horizontalOffset = 70f
         val verticalOffset = 50f
 
+        val cx = canvasWidth / 2 - horizontalOffset
+        val cy = canvasHeight / 2 + verticalOffset
+        val srcPath = createPath(cx, cy, 5, radius)
+
         with(drawContext.canvas.nativeCanvas) {
             val checkPoint = saveLayer(null, null)
 
-            drawRect(
-                srcColor,
-                topLeft = Offset(
-                    (canvasWidth - 2 * radius) / 2 - horizontalOffset,
-                    (canvasHeight - 2 * radius) / 2 - verticalOffset
-                ),
-                size = Size(2 * radius, 2 * radius)
-            )
-
             drawCircle(
-                blendMode = blendMode,
                 color = dstColor,
                 radius = radius,
                 center = Offset(
                     canvasWidth / 2 + horizontalOffset,
-                    canvasHeight / 2 + verticalOffset
+                    canvasHeight / 2 - verticalOffset
                 ),
             )
+            drawPath(path = srcPath, color = srcColor, blendMode = blendMode)
+
             restoreToCount(checkPoint)
         }
     }
@@ -165,86 +175,128 @@ private fun BlendModeExample() {
     }
 }
 
+
 @Composable
-private fun BlendModeSelection(
-    modifier: Modifier = Modifier,
-    selectedIndex: Int,
-    onBlendModeSelected: (Int, BlendMode) -> Unit
-) {
-    val radioOptions = blendModes.keys.toList()
+private fun DrawImageBlendMode() {
 
+    var selectedIndex by remember { mutableStateOf(3) }
+    var blendMode: BlendMode by remember { mutableStateOf(BlendMode.SrcOver) }
 
-    val (selectedOption: String, onOptionSelected: (String) -> Unit) = remember {
-        mutableStateOf(
-            radioOptions[selectedIndex]
-        )
-    }
-// Note that Modifier.selectableGroup() is essential to ensure correct accessibility behavior
-    Column(
-        Modifier
-            .selectableGroup()
-            .then(modifier)
-    ) {
-        radioOptions.forEachIndexed { index, text ->
-            Row(
-                Modifier
-                    .fillMaxWidth()
-                    .selectable(
-                        selected = (text == selectedOption),
-                        onClick = {
-                            onOptionSelected(text)
-                            onBlendModeSelected(index, blendModes[text]!!)
-                        },
-                        role = Role.RadioButton
-                    )
-                    .padding(8.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                RadioButton(
-                    selected = (text == selectedOption),
-                    onClick = null
-                )
-                Text(
-                    text = text,
-                    style = MaterialTheme.typography.body1.merge(),
-                    modifier = Modifier.padding(start = 2.dp)
-                )
-            }
+    val srcImage = ImageBitmap.imageResource(id = R.drawable.composite_src)
+    val dstImage = ImageBitmap.imageResource(id = R.drawable.composite_dst)
+
+    Canvas(modifier = canvasModifier) {
+
+        val canvasWidth = size.width.roundToInt()
+        val canvasHeight = size.height.roundToInt()
+
+        with(drawContext.canvas.nativeCanvas) {
+            val checkPoint = saveLayer(null, null)
+
+            drawImage(
+                image = dstImage,
+                srcSize = IntSize(canvasWidth / 2, canvasHeight / 2),
+                dstSize = IntSize(canvasWidth, canvasHeight),
+            )
+            drawImage(
+                image = srcImage,
+                srcSize = IntSize(canvasWidth / 2, canvasHeight / 2),
+                dstSize = IntSize(canvasWidth, canvasHeight),
+                blendMode = blendMode
+            )
+            restoreToCount(checkPoint)
         }
     }
+
+    Text(
+        text = "Dst BlendMode: $blendMode",
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(8.dp)
+    )
+
+    BlendModeSelection(
+        modifier = Modifier
+            .height(200.dp)
+            .verticalScroll(rememberScrollState()),
+        selectedIndex = selectedIndex,
+        onBlendModeSelected = { index, mode ->
+            blendMode = mode
+            selectedIndex = index
+        }
+    )
 }
 
-val blendModes = hashMapOf(
-    "Clear" to BlendMode.Clear,
-    "Src" to BlendMode.Src,
-    "Dst" to BlendMode.Dst,
-    "SrcOver" to BlendMode.SrcOver,
-    "DstOver" to BlendMode.DstOver,
-    "SrcIn" to BlendMode.SrcIn,
-    "DstIn" to BlendMode.DstIn,
-    "SrcOut" to BlendMode.SrcOut,
-    "DstOut" to BlendMode.DstOut,
-    "SrcAtop" to BlendMode.SrcAtop,
-    "DstAtop" to BlendMode.DstAtop,
-    "Xor" to BlendMode.Xor,
-    "Plus" to BlendMode.Plus,
-    "Modulate" to BlendMode.Modulate,
-    "Screen" to BlendMode.Screen,
-    "Overlay" to BlendMode.Overlay,
-    "Darken" to BlendMode.Darken,
-    "Lighten" to BlendMode.Lighten,
-    "ColorDodge" to BlendMode.ColorDodge,
-    "ColorBurn" to BlendMode.ColorBurn,
-    "HardLight" to BlendMode.Hardlight,
-    "SoftLight" to BlendMode.Softlight,
-    "Difference" to BlendMode.Difference,
-    "Exclusion" to BlendMode.Exclusion,
-    "Multiply" to BlendMode.Multiply,
-    "Hue" to BlendMode.Hue,
-    "Saturation" to BlendMode.Saturation,
-    "Color" to BlendMode.Color,
-    "Luminosity" to BlendMode.Luminosity
-)
+@Composable
+private fun ClipImageWithBlendMode() {
+    var sides by remember { mutableStateOf(6f) }
+    val bitmap = ImageBitmap.imageResource(id = R.drawable.landscape1)
+
+    var selectedIndex by remember { mutableStateOf(5) }
+    var blendMode: BlendMode by remember { mutableStateOf(BlendMode.SrcIn) }
+
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            .height(200.dp)
+    ) {
+        val canvasWidth = size.width.roundToInt()
+        val canvasHeight = size.height.roundToInt()
+        val cx = canvasWidth / 2
+        val cy = canvasHeight / 2
+        val radius = (canvasHeight - 20.dp.toPx()) / 2
+        val path = createPath(cx.toFloat(), cy.toFloat(), sides.roundToInt(), radius)
+
+
+
+        with(drawContext.canvas.nativeCanvas) {
+            val checkPoint = saveLayer(null, null)
+
+            drawPath(
+                color = Color.Blue,
+                path = path
+            )
+
+            drawImage(
+                blendMode = blendMode,
+                image = bitmap,
+                srcSize = IntSize(canvasWidth / 2, canvasHeight / 2),
+                dstSize = IntSize(canvasWidth, canvasHeight),
+            )
+
+            restoreToCount(checkPoint)
+        }
+    }
+
+    Column(modifier = Modifier.padding(horizontal = 20.dp)) {
+
+        Text(text = "Sides ${sides.roundToInt()}")
+        Slider(
+            value = sides,
+            onValueChange = { sides = it },
+            valueRange = 3f..12f,
+            steps = 10
+        )
+
+        Text(
+            text = "Dst BlendMode: $blendMode",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(8.dp)
+        )
+
+        BlendModeSelection(
+            modifier = Modifier
+                .height(200.dp)
+                .verticalScroll(rememberScrollState()),
+            selectedIndex = selectedIndex,
+            onBlendModeSelected = { index, mode ->
+                blendMode = mode
+                selectedIndex = index
+            }
+        )
+    }
+}
 
 private val canvasModifier = Modifier
     .background(Color.LightGray)
