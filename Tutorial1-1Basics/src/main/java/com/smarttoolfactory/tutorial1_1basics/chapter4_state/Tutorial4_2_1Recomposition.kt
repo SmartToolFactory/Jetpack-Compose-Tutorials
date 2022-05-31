cae73f2aa08636a334f0c5f47af387895234f5b0
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-05-31 20:24:01 +0300

    update recomposition sample

--- Tutorial1-1Basics/src/main/java/com/smarttoolfactory/tutorial1_1basics/chapter4_state/Tutorial4_2_1Recomposition.kt
+++ Tutorial1-1Basics/src/main/java/com/smarttoolfactory/tutorial1_1basics/chapter4_state/Tutorial4_2_1Recomposition.kt
@@ -1,62 +1,23 @@
 package com.smarttoolfactory.tutorial1_1basics.chapter4_state
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.smarttoolfactory.tutorial1_1basics.ui.Blue400
-import com.smarttoolfactory.tutorial1_1basics.ui.Orange400
-import com.smarttoolfactory.tutorial1_1basics.ui.Pink400
+import androidx.compose.ui.unit.sp
 import com.smarttoolfactory.tutorial1_1basics.ui.components.StyleableTutorialText
+import com.smarttoolfactory.tutorial1_1basics.ui.components.getRandomColor
 
 /*
-    This tutorial uses references from this medium article. Check it out.
-    https://medium.com/mobile-app-development-publication/android-jetpack-compose-recomposition-made-easy-527ecc9bcbaf
+    This tutorial uses this article as reference to show how scoping effect which section of
+    any composable should be recomposed
+
+    https://www.jetpackcompose.app/articles/donut-hole-skipping-in-jetpack-compose
  */
-
-/*
-    Buttons
- */
-var update1 by mutableStateOf(0)
-var update2 by mutableStateOf(0)
-var update3 by mutableStateOf(0)
-
-
-/*
-    Flat design
- */
-var recomposeFlatCombined1 = 0
-var recomposeFlatCombined2 = 0
-var recomposeFlatCombined3 = 0
-
-var recomposeFlatTop = 0
-var recomposeFlatMiddle = 0
-var recomposeFlatBottom = 0
-
-/*
-    Hierarchical design
- */
-var recomposeHierarchical1 = 0
-var recomposeHierarchical2 = 0
-var recomposeHierarchical3 = 0
-
-var recomposeHierarchicalOuter = 0
-var recomposeHierarchicalCenter = 0
-var recomposeHierarchicalInner = 0
 
 @Composable
 fun Tutorial4_2_1Screen() {
@@ -67,302 +28,90 @@ fun Tutorial4_2_1Screen() {
 private fun TutorialContent() {
     Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .verticalScroll(rememberScrollState())
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
 
-        UpdateButtons()
-
+        Spacer(modifier = Modifier.height(20.dp))
         StyleableTutorialText(
-            text = "**FlatCombined:** Composables are siblings. **Every Composable** gets recomposed.",
+            text = "Recomposition happens in closest scope that reads any **State** change. In this" +
+                    "example after composition on each recomposition only scopes that read a " +
+                    "value are recomposed skipping recomposition of Composables between.",
             bullets = false
         )
-        FlatCombined()
-        StyleableTutorialText(
-            text = "**FlatSeparated:** Composables are sibling. **Only required Composable** gets recomposed.",
-            bullets = false
-        )
-        FlatSeparated()
-        StyleableTutorialText(
-            text = "**HierarchicalCombined:** Composables are parent and children. **Every Composable** gets recomposed.",
-            bullets = false
-        )
-        HierarchicalCombined()
-        StyleableTutorialText(
-            text = "**HierarchicalSeparated:**  Composables are parent and children. **Only required Composable** gets recomposed.",
-            bullets = false
-        )
-        HierarchicalSeparated()
+        Spacer(modifier = Modifier.height(20.dp))
+        MyComponent()
     }
 }
 
 @Composable
-private fun FlatSeparated() {
-    Column(Modifier.padding(horizontal = 8.dp)) {
-        FlatTopLayer()
-        FlatMiddleLayer()
-        FlatBottomLayer()
+fun MyComponent() {
+    var counter by remember { mutableStateOf(0) }
+
+    LogCompositions("🔥 MyComposable function")
+
+    CustomButton(onClick = { counter++ }) {
+        LogCompositions("🍉 CustomButton scope")
+        CustomTextWrapper(modifier = Modifier, text = "Counter: $counter")
     }
 }
 
 @Composable
-private fun FlatTopLayer() {
-    Column(
+fun CustomButton(
+    onClick: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    LogCompositions("🌽 CustomButton function")
+    Button(
+        onClick = onClick,
         modifier = Modifier
-            .fillMaxWidth()
-            .background(color = Orange400)
+            .border(4.dp, getRandomColor())
+            .padding(16.dp)
     ) {
-
-        recomposeFlatTop++
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = "Recompose: $recomposeFlatTop; Update: $update1",
-            textAlign = TextAlign.Center
-        )
+        LogCompositions("🍏 Button function")
+        content()
     }
 }
 
 @Composable
-private fun FlatMiddleLayer() {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(color = Blue400)
-    ) {
-
-        recomposeFlatMiddle++
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = "Recompose: $recomposeFlatMiddle; Update: $update2",
-            textAlign = TextAlign.Center
-        )
-    }
+fun CustomTextWrapper(
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    LogCompositions("🎃 CustomTextWrapper function")
+    AnotherComposable()
+    CustomText(text, modifier)
 }
 
 @Composable
-private fun FlatBottomLayer() {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(color = Pink400)
-    ) {
-
-        recomposeFlatBottom++
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = "Recompose: $recomposeFlatBottom; Update: $update3",
-            textAlign = TextAlign.Center
-        )
-    }
+private fun AnotherComposable(){
+    LogCompositions("🍋 AnotherComposable function")
 }
 
 @Composable
-private fun FlatCombined() {
-    Column(
-        modifier = Modifier.padding(horizontal = 8.dp)
-    ) {
+fun CustomText(
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    LogCompositions("🍎 CustomText function")
 
-        Column(Modifier.background(color = Orange400)) {
-            recomposeFlatCombined1++
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                text = "Recompose: $recomposeFlatCombined1; Update: $update1",
-                textAlign = TextAlign.Center
-            )
-        }
-
-        Column {
-            recomposeFlatCombined2++
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(color = Blue400),
-                text = "Recompose: $recomposeFlatCombined2; Update: $update2",
-                textAlign = TextAlign.Center
-            )
-        }
-
-        Column {
-            recomposeFlatCombined3++
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(color = Pink400),
-                text = "Recompose: $recomposeFlatCombined3; Update: $update3",
-                textAlign = TextAlign.Center
-            )
-        }
-    }
+    Text(
+        text = text,
+        modifier = modifier
+            .border(3.dp, getRandomColor())
+            .padding(32.dp),
+        fontSize = 20.sp
+    )
 }
 
-/*
-    Hierarchical Structure
- */
+class Ref(var value: Int)
+
+// Note the inline function below which ensures that this function is essentially
+// copied at the call site to ensure that its logging only recompositions from the
+// original call site.
 @Composable
-private fun HierarchicalCombined() {
-    Column(
-        modifier = Modifier
-            .padding(horizontal = 8.dp)
-            .fillMaxWidth()
-            .shadow(2.dp, RoundedCornerShape(8.dp), clip = true)
-            .background(color = Orange400)
-
-
-    ) {
-
-        recomposeHierarchical1++
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = "Recompose: $recomposeHierarchical1; Update: $update1",
-            textAlign = TextAlign.Center
-        )
-        Column(
-            modifier = Modifier
-                .padding(8.dp)
-                .fillMaxWidth()
-                .shadow(2.dp, RoundedCornerShape(8.dp), clip = true)
-                .background(color = Blue400)
-        ) {
-
-            recomposeHierarchical2++
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                text = "Recompose: $recomposeHierarchical2; Update: $update2",
-                textAlign = TextAlign.Center
-            )
-
-            Column(
-                modifier = Modifier
-                    .padding(8.dp)
-                    .fillMaxWidth()
-                    .shadow(2.dp, RoundedCornerShape(8.dp), clip = true)
-                    .background(color = Pink400)
-
-
-            ) {
-
-                recomposeHierarchical3++
-                Text(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = "Recompose: $recomposeHierarchical3; Update: $update3",
-                    textAlign = TextAlign.Center
-                )
-            }
-        }
-    }
-}
-
-@Composable
-private fun HierarchicalSeparated() {
-    HierarchicalOuterLayer()
-}
-
-@Composable
-private fun HierarchicalOuterLayer() {
-    Column(
-        modifier = Modifier
-            .padding(horizontal = 8.dp)
-            .fillMaxWidth()
-            .shadow(2.dp, RoundedCornerShape(8.dp), clip = true)
-            .background(color = Orange400)
-    ) {
-
-        recomposeHierarchicalOuter++
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = "Recompose: $recomposeHierarchicalOuter; Update: $update1",
-            textAlign = TextAlign.Center
-        )
-
-        HierarchicalCenterLayer()
-    }
-}
-
-@Composable
-private fun HierarchicalCenterLayer() {
-    Column(
-        modifier = Modifier
-            .padding(8.dp)
-            .fillMaxWidth()
-            .shadow(2.dp, RoundedCornerShape(8.dp), clip = true)
-            .background(color = Blue400)
-
-    ) {
-
-        recomposeHierarchicalCenter++
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = "Recompose: $recomposeHierarchicalCenter; Update: $update2",
-            textAlign = TextAlign.Center
-        )
-
-        HierarchicalInnerLayer()
-    }
-}
-
-@Composable
-private fun HierarchicalInnerLayer() {
-    Column(
-        modifier = Modifier
-            .padding(8.dp)
-            .fillMaxWidth()
-            .shadow(2.dp, RoundedCornerShape(8.dp), clip = true)
-            .background(color = Pink400)
-    ) {
-
-        recomposeHierarchicalInner++
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = "Recompose: $recomposeHierarchicalInner; Update: $update3",
-            textAlign = TextAlign.Center
-        )
-    }
-}
-
-@Composable
-private fun UpdateButtons() {
-    Column {
-
-        Button(
-            colors = ButtonDefaults.buttonColors(backgroundColor = Orange400),
-            modifier = Modifier
-                .padding(start = 8.dp, end = 8.dp, top = 4.dp)
-                .fillMaxWidth(),
-            onClick = { update1++ },
-            shape = RoundedCornerShape(5.dp)
-        ) {
-            Text(
-                text = "Update: $update1",
-                textAlign = TextAlign.Center
-            )
-        }
-
-        Button(
-            colors = ButtonDefaults.buttonColors(backgroundColor = Blue400),
-            modifier = Modifier
-                .padding(start = 8.dp, end = 8.dp, top = 2.dp)
-                .fillMaxWidth(),
-            onClick = { update2++ },
-            shape = RoundedCornerShape(5.dp)
-        ) {
-            Text(
-                text = "Update: $update2",
-                textAlign = TextAlign.Center
-            )
-        }
-
-        Button(
-            colors = ButtonDefaults.buttonColors(backgroundColor = Pink400),
-            modifier = Modifier
-                .padding(start = 8.dp, end = 8.dp, top = 2.dp)
-                .fillMaxWidth(),
-            onClick = { update3++ },
-            shape = RoundedCornerShape(5.dp)
-        ) {
-            Text(
-                text = "Update: $update3",
-                textAlign = TextAlign.Center
-            )
-        }
-    }
+inline fun LogCompositions(msg: String) {
+    val ref = remember { Ref(0) }
+    SideEffect { ref.value++ }
+    println("$msg ${ref.value}")
 }
