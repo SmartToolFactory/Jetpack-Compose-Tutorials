5cd66ade13c2ca8dfcc5fd6c7cd5531f9b7b71bb
Smart Tool Factory <smarttoolfactory@icloud.com>
2023-03-25 15:43:40 +0300

    formatting

--- Tutorial1-1Basics/src/main/java/com/smarttoolfactory/tutorial1_1basics/chapter6_graphics/Tutorial6_13BorderProgressTimer.kt
+++ Tutorial1-1Basics/src/main/java/com/smarttoolfactory/tutorial1_1basics/chapter6_graphics/Tutorial6_13BorderProgressTimer.kt
@@ -4,10 +4,22 @@ import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
@@ -226,10 +238,10 @@ private fun TutorialContent() {
         Button(
             modifier = Modifier.fillMaxWidth(),
             onClick = {
-                if (currentTime>0){
+                if (currentTime > 0) {
                     targetValue = 0f
                     timerStarted = true
-                }else {
+                } else {
                     currentTime = startDurationInSeconds
                     timerStarted = true
                 }
@@ -240,7 +252,7 @@ private fun TutorialContent() {
         Text(
             text = "currentTime: $currentTime, " +
                     "progress: ${progress.toInt()}",
-            color = if(progress == 0f) Color.DarkGray else Color.Red
+            color = if (progress == 0f) Color.DarkGray else Color.Red
         )
 
     }
