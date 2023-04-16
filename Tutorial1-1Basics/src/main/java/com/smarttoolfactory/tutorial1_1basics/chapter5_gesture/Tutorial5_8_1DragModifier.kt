b4371a96365482d8abaf15ff2498eb0b000d9ba9
Smart Tool Factory <smarttoolfactory@icloud.com>
2023-04-16 15:10:18 +0300

    update draggable sample

--- Tutorial1-1Basics/src/main/java/com/smarttoolfactory/tutorial1_1basics/chapter5_gesture/Tutorial5_8_1DragModifier.kt
+++ Tutorial1-1Basics/src/main/java/com/smarttoolfactory/tutorial1_1basics/chapter5_gesture/Tutorial5_8_1DragModifier.kt
@@ -4,9 +4,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.rememberDraggableState
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.IntOffset
@@ -47,6 +56,16 @@ private fun TutorialContent() {
         var offsetX by remember(orientation) { mutableStateOf(0f) }
         var offsetY by remember(orientation) { mutableStateOf(0f) }
 
+        val state = if (orientation == Orientation.Horizontal) {
+            rememberDraggableState { delta ->
+                offsetX += delta
+            }
+        } else {
+            rememberDraggableState { delta ->
+                offsetY += delta
+            }
+        }
+
         ExposedSelectionMenu(title = "Orientation",
             index = when (orientation) {
                 Orientation.Horizontal -> 0
@@ -74,13 +93,7 @@ private fun TutorialContent() {
                 // it on screen by, for example, moving the element via the offset modifier:
                 .draggable(
                     orientation = orientation,
-                    state = rememberDraggableState { delta ->
-                        if (orientation == Orientation.Horizontal) {
-                            offsetX += delta
-                        } else {
-                            offsetY += delta
-                        }
-                    }
+                    state = state
                 )
                 .background(Blue400)
                 .padding(20.dp),
