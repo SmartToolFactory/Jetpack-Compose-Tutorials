63795c733b9ecdb23810ce66cb60608954f03982
Smart Tool Factory <smarttoolfactory@icloud.com>
2023-10-21 12:43:42 +0300

    update tab tutorial

--- Tutorial1-1Basics/src/main/java/com/smarttoolfactory/tutorial1_1basics/chapter2_material_widgets/Tutorial2_6_2Tab.kt
+++ Tutorial1-1Basics/src/main/java/com/smarttoolfactory/tutorial1_1basics/chapter2_material_widgets/Tutorial2_6_2Tab.kt
@@ -1,15 +1,21 @@
 package com.smarttoolfactory.tutorial1_1basics.chapter2_material_widgets
 
 import android.content.res.Configuration
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -31,9 +37,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.debugInspectorInfo
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,13 +52,23 @@ import com.smarttoolfactory.tutorial1_1basics.ui.ComposeTutorialsTheme
 fun TextTabComponent() {
     var selectedIndex by remember { mutableStateOf(0) }
 
-    val list = listOf("Home", "Map", "Settings")
+    val list = listOf("Home", "Map Screen", "Settings")
 
     TabRow(selectedTabIndex = selectedIndex,
         indicator = { tabPositions: List<TabPosition> ->
+
+            tabPositions.forEachIndexed { index, tabPosition ->
+                println(
+                    "index: $index, " +
+                            "left: ${tabPosition.left}, " +
+                            "right: ${tabPosition.right}, " +
+                            "width: ${tabPosition.width}"
+                )
+            }
+
             Box(
                 modifier = Modifier
-                    .tabIndicatorOffset(tabPositions[selectedIndex])
+                    .myTabIndicatorOffset(tabPositions[selectedIndex])
                     .height(4.dp)
                     .background(color = Color.Red)
             ) {}
@@ -62,6 +80,30 @@ fun TextTabComponent() {
             )
         }
     }
+}
+
+fun Modifier.myTabIndicatorOffset(
+    currentTabPosition: TabPosition
+): Modifier = composed(
+    inspectorInfo = debugInspectorInfo {
+        name = "tabIndicatorOffset"
+        value = currentTabPosition
+    }
+) {
+    val currentTabWidth by animateDpAsState(
+        targetValue = currentTabPosition.width,
+        animationSpec = tween(durationMillis = 5250, easing = FastOutSlowInEasing),
+        label = "tab width"
+    )
+    val indicatorOffset by animateDpAsState(
+        targetValue = currentTabPosition.left,
+        animationSpec = tween(durationMillis = 250, easing = FastOutSlowInEasing),
+        label = "indicator offset"
+    )
+    fillMaxWidth()
+        .wrapContentSize(Alignment.BottomStart)
+        .offset(x = indicatorOffset)
+        .width(currentTabWidth)
 }
 
 @Preview
