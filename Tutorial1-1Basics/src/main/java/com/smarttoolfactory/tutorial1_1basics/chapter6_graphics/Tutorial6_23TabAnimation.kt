fc350644563be042af4532681e43ec934263d991
Smart Tool Factory <smarttoolfactory@icloud.com>
2023-10-21 18:50:33 +0300

    add tab animation tutorial

---
+++ Tutorial1-1Basics/src/main/java/com/smarttoolfactory/tutorial1_1basics/chapter6_graphics/Tutorial6_23TabAnimation.kt
@@ -0,0 +1,205 @@
+package com.smarttoolfactory.tutorial1_1basics.chapter6_graphics
+
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.smarttoolfactory.tutorial1_1basics.ui.components.TutorialHeader
+
+@Preview
+@Composable
+fun Tutorial6_23Screen() {
+    TutorialContent()
+}
+
+@Composable
+private fun TutorialContent() {
+    Column(modifier = Modifier.padding(8.dp)) {
+        TutorialHeader(text = "Animated Switch/Tab")
+        TextSwitchTest()
+    }
+}
+
+@Preview
+@Composable
+private fun TextSwitchTest() {
+    val items = remember {
+        listOf("Left", "Right")
+    }
+
+    var selectedIndex by remember {
+        mutableStateOf(0)
+    }
+
+
+    Column {
+        TextSwitch(
+            selectedIndex = selectedIndex,
+            items = items,
+            onSelectionChange = {
+                selectedIndex = it
+            }
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        TextSwitch(
+            selectedIndex = selectedIndex,
+            items = items,
+            animationSpec = tween(durationMillis = 2500, easing = LinearEasing),
+            onSelectionChange = {
+                selectedIndex = it
+            }
+        )
+    }
+}
+
+@Immutable
+private class TextSwitchColors(
+    val backgroundColor: Color = Color(0xfff3f3f2),
+    val contentColor: Color = Color.White,
+    val textColor: Color = Color.Gray,
+    val selectedTextColor: Color = Color.Black
+)
+
+@Composable
+private fun TextSwitch(
+    modifier: Modifier = Modifier,
+    selectedIndex: Int,
+    items: List<String>,
+    colors: TextSwitchColors = TextSwitchColors(),
+    animationSpec: AnimationSpec<Dp> = tween(durationMillis = 250, easing = FastOutSlowInEasing),
+    onSelectionChange: (Int) -> Unit
+) {
+
+    BoxWithConstraints(
+        modifier
+            .padding(8.dp)
+            .height(56.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .background(colors.backgroundColor)
+            .padding(8.dp)
+    ) {
+        if (items.isNotEmpty()) {
+
+            val maxWidth = this.maxWidth
+            val tabWidth = maxWidth / items.size
+
+            val indicatorOffset by animateDpAsState(
+                targetValue = tabWidth * selectedIndex,
+                animationSpec = animationSpec,
+                label = "indicator offset"
+            )
+
+            // This is for shadow layer matching white background
+            Box(
+                modifier = Modifier
+                    .offset(x = indicatorOffset)
+                    .shadow(4.dp, RoundedCornerShape(8.dp))
+                    .width(tabWidth)
+                    .fillMaxHeight()
+            )
+
+            Row(modifier = Modifier
+                .fillMaxWidth()
+
+                .drawWithContent {
+
+                    // This is for setting black tex while drawing on white background
+                    val padding = 8.dp.toPx()
+                    drawRoundRect(
+                        topLeft = Offset(x = indicatorOffset.toPx() + padding, padding),
+                        size = Size(size.width / 2 - padding * 2, size.height - padding * 2),
+                        color = colors.selectedTextColor,
+                        cornerRadius = CornerRadius(x = 8.dp.toPx(), y = 8.dp.toPx()),
+                    )
+
+                    drawWithLayer {
+                        drawContent()
+
+                        // This is white top rounded rectangle
+                        drawRoundRect(
+                            topLeft = Offset(x = indicatorOffset.toPx(), 0f),
+                            size = Size(size.width / 2, size.height),
+                            color = colors.contentColor,
+                            cornerRadius = CornerRadius(x = 8.dp.toPx(), y = 8.dp.toPx()),
+                            blendMode = BlendMode.SrcOut
+                        )
+                    }
+
+                }
+            ) {
+                items.forEachIndexed { index, text ->
+                    Box(
+                        modifier = Modifier
+                            .width(tabWidth)
+                            .fillMaxHeight()
+                            .clickable(
+                                interactionSource = remember {
+                                    MutableInteractionSource()
+                                },
+                                indication = null,
+                                onClick = {
+                                    onSelectionChange(index)
+                                }
+                            ),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = text,
+                            fontSize = 20.sp,
+                            color = colors.textColor
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+private fun ContentDrawScope.drawWithLayer(block: ContentDrawScope.() -> Unit) {
+    with(drawContext.canvas.nativeCanvas) {
+        val checkPoint = saveLayer(null, null)
+        block()
+        restoreToCount(checkPoint)
+    }
+}
