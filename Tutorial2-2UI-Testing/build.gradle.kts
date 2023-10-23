e2ce25307d4c992c450495d92136f80b20389986
Smart Tool Factory <smarttoolfactory@icloud.com>
2023-10-23 15:45:18 +0300

    update compose version to 1.5.4

--- Tutorial2-2UI-Testing/build.gradle.kts
+++ Tutorial2-2UI-Testing/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
