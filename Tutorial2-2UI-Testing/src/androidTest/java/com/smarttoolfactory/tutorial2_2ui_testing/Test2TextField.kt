29341a1c2dcb49e7aa850a3b02be2d039c5360a1
Smart Tool Factory <smarttoolfactory@icloud.com>
2023-11-19 21:49:32 +0300

    add TextField tests

--- Tutorial2-2UI-Testing/src/androidTest/java/com/smarttoolfactory/tutorial2_2ui_testing/Test2TextField.kt
+++ Tutorial2-2UI-Testing/src/androidTest/java/com/smarttoolfactory/tutorial2_2ui_testing/Test2TextField.kt
@@ -2,6 +2,7 @@ package com.smarttoolfactory.tutorial2_2ui_testing
 
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -14,7 +15,9 @@ import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performTextClearance
 import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.test.performTextReplacement
 import org.junit.Rule
 import org.junit.Test
 
@@ -24,28 +27,10 @@ class Test2TextField {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun enter_text_TextFieldTest() {
+    fun performTextInput_TextField_Test() {
 
         composeTestRule.setContent {
-            var text by remember {
-                mutableStateOf("")
-            }
-
-            TextField(
-                modifier = Modifier.semantics {
-                    contentDescription = "input"
-                },
-                label = {
-                    Text("Label Text")
-                },
-                placeholder = {
-                    Text("Placeholder Text")
-                },
-                value = text,
-                onValueChange = {
-                    text = it
-                }
-            )
+            TextFieldSample()
         }
 
         // set TextField text
@@ -70,7 +55,115 @@ class Test2TextField {
             .assertTextEquals("Label Text", "100")
 
         // 🔥 This is for displaying TextField, do not use sleep in actual tests
-        Thread.sleep(2000)
+        Thread.sleep(1000)
     }
 
+    @Test
+    fun performTextInput_twice_TextField_Test() {
+
+        composeTestRule.setContent {
+            TextFieldSample()
+        }
+
+        // set TextField text
+        composeTestRule
+            .onNodeWithContentDescription("input")
+            // Sends the given text to this node in similar way to IME.
+            .performTextInput("100")
+
+
+        // add another char to TextField text
+        composeTestRule
+            .onNodeWithContentDescription("input")
+            // Sends the given text to this node in similar way to IME.
+            .performTextInput("1")
+
+        // assert that text is set
+        composeTestRule.onNodeWithContentDescription("input")
+            .assert(hasText("1001", ignoreCase = true))
+
+        // 🔥 This is for displaying TextField, do not use sleep in actual tests
+        Thread.sleep(1000)
+    }
+
+    @Test
+    fun performTextReplacement_TextField_Test() {
+
+        composeTestRule.setContent {
+            TextFieldSample()
+        }
+
+        // set TextField text
+        composeTestRule
+            .onNodeWithContentDescription("input")
+            // Sends the given text to this node in similar way to IME.
+            .performTextInput("100")
+
+
+        // replace TextField text
+        composeTestRule
+            .onNodeWithContentDescription("input")
+            // Replaces existing text with the given text in this node in similar way to IME.
+            //This does not reflect text selection. All the text gets cleared out and new inserted.
+            .performTextReplacement("200")
+
+        // assert that text is set
+        composeTestRule.onNodeWithContentDescription("input")
+            .assert(hasText("200", ignoreCase = true))
+
+        // 🔥 This is for displaying TextField, do not use sleep in actual tests
+        Thread.sleep(1000)
+    }
+
+    @Test
+    fun performTextClearance_TextField_Test() {
+
+        composeTestRule.setContent {
+            TextFieldSample()
+        }
+
+        // set TextField text
+        composeTestRule
+            .onNodeWithContentDescription("input")
+            // Sends the given text to this node in similar way to IME.
+            .performTextInput("100")
+
+
+        // replace TextField text
+        composeTestRule
+            .onNodeWithContentDescription("input")
+            // Clears the text in this node in similar way to IME.
+            .performTextClearance()
+
+        // assert that text is set
+        composeTestRule.onNodeWithContentDescription("input")
+            .assert(hasText("", ignoreCase = true))
+
+        // 🔥 This is for displaying TextField, do not use sleep in actual tests
+        Thread.sleep(1000)
+    }
+
+}
+
+@Composable
+private fun TextFieldSample() {
+    var text by remember {
+        mutableStateOf("")
+    }
+
+    TextField(
+        modifier = Modifier.semantics {
+            contentDescription = "input"
+        },
+        label = {
+            Text("Label Text")
+        },
+        placeholder = {
+            Text("Placeholder Text")
+        },
+        value = text,
+        onValueChange = {
+            text = it
+        }
+    )
 }
