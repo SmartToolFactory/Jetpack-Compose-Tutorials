7fe1cbd16451d059e8b820af3f8d461279c491e3
Smart Tool Factory <smarttoolfactory@icloud.com>
2024-09-11 23:00:26 +0300

    add SharedElementTransition9Pager sample

---
+++ Tutorial1-1Basics/src/main/java/com/smarttoolfactory/tutorial1_1basics/chapter9_animation/SharedElementTransition9Pager.kt
@@ -0,0 +1,231 @@
+@file:OptIn(ExperimentalSharedTransitionApi::class)
+
+package com.smarttoolfactory.tutorial1_1basics.chapter9_animation
+
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionLayout
+import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+
+
+@Preview
+@Composable
+fun SharedElement_Pager() {
+
+    val scrollState = rememberLazyListState()
+
+    SharedTransitionLayout {
+        val navController = rememberNavController()
+        NavHost(
+            navController = navController,
+            startDestination = "home"
+        ) {
+
+            composable("home") {
+                HomeScreen(
+                    navController = navController,
+                    scrollState = scrollState,
+                    sharedTransitionScope = this@SharedTransitionLayout,
+                    animatedContentScope = this@composable,
+                )
+            }
+
+            composable(
+                "details/{item}",
+                arguments = listOf(navArgument("item") { type = NavType.IntType })
+            ) { backStackEntry ->
+                val id = backStackEntry.arguments?.getInt("item")
+                val snack = listSnacks[id!!]
+                DetailsScreen(
+                    navController,
+                    id,
+                    snack,
+                    sharedTransitionScope = this@SharedTransitionLayout,
+                    animatedContentScope = this@composable
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun DetailsScreen(
+    navController: NavHostController,
+    id: Int,
+    snack: SnackItem,
+    sharedTransitionScope: SharedTransitionScope,
+    animatedContentScope: AnimatedContentScope,
+) {
+
+    var selectedIndex by remember {
+        mutableStateOf(id)
+    }
+    with(sharedTransitionScope) {
+
+        val index = listSnacks.indexOf(snack).coerceAtLeast(0)
+
+        val pagerState = rememberPagerState(
+            initialPage = index,
+            pageCount = {
+                listSnacks.size
+            }
+        )
+
+        HorizontalPager(
+            state = pagerState,
+            pageSpacing = 16.dp
+        ) { page ->
+
+            selectedIndex = page
+            val currentSnack = listSnacks[page]
+
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .clickable(
+                        interactionSource = remember {
+                            MutableInteractionSource()
+                        },
+                        indication = null
+                    ) {
+                        navController.navigate("home")
+                    }
+            ) {
+                Image(
+                    painterResource(id = currentSnack.image),
+                    contentDescription = currentSnack.description,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .sharedElement(
+                            sharedTransitionScope.rememberSharedContentState(key = "image-$selectedIndex"),
+                            animatedVisibilityScope = animatedContentScope
+                        )
+                        .aspectRatio(1f)
+                        .fillMaxWidth()
+                )
+                Text(
+                    currentSnack.name, fontSize = 38.sp,
+                    modifier =
+                    Modifier
+                        .sharedBounds(
+                            sharedTransitionScope.rememberSharedContentState(key = "text-$selectedIndex"),
+                            animatedVisibilityScope = animatedContentScope,
+                        )
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun HomeScreen(
+    navController: NavHostController,
+    scrollState: LazyListState,
+    sharedTransitionScope: SharedTransitionScope,
+    animatedContentScope: AnimatedContentScope,
+) {
+
+    var isDetailSelected by remember {
+        mutableStateOf(false)
+    }
+
+    with(sharedTransitionScope) {
+        Column {
+
+            TopAppBar(
+                title = {
+                    Text("Title")
+                },
+                backgroundColor = Color.Black,
+                contentColor = Color.White,
+                modifier = if (isDetailSelected) Modifier
+                else Modifier.renderInSharedTransitionScopeOverlay()
+            )
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentPadding = PaddingValues(8.dp),
+                state = scrollState,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                itemsIndexed(listSnacks) { index, item ->
+                    Row(
+                        Modifier.clickable(
+                            interactionSource = remember {
+                                MutableInteractionSource()
+                            },
+                            indication = null
+                        ) {
+                            isDetailSelected = true
+                            navController.navigate("details/$index")
+                        }
+                    ) {
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Image(
+                            painterResource(id = item.image),
+                            contentDescription = item.description,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .sharedElement(
+                                    sharedTransitionScope.rememberSharedContentState(key = "image-$index"),
+                                    animatedVisibilityScope = animatedContentScope
+                                )
+                                .size(120.dp)
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(
+                            item.name, fontSize = 18.sp,
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .sharedBounds(
+                                    sharedTransitionScope.rememberSharedContentState(key = "text-$index"),
+                                    animatedVisibilityScope = animatedContentScope
+                                )
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+}
