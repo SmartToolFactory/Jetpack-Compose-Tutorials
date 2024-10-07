bd634f9bb7557e4379a9f1b39597e622e1837f73
Smart Tool Factory <smarttoolfactory@icloud.com>
2024-10-07 21:53:28 +0300

    add navigation compose to gradle

--- Tutorial3-1Navigation/build.gradle.kts
+++ Tutorial3-1Navigation/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit.v121)
     androidTestImplementation(libs.androidx.espresso.core.v361)
