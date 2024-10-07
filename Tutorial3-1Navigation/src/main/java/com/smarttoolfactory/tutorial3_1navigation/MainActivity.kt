a5419df4e3db02f3279b1f641a0b248dd865a713
Smart Tool Factory <smarttoolfactory@icloud.com>
2024-10-07 21:43:53 +0300

    create navigation tutorial

---
+++ Tutorial3-1Navigation/src/main/java/com/smarttoolfactory/tutorial3_1navigation/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.smarttoolfactory.tutorial3_1navigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.smarttoolfactory.tutorial3_1navigation.ui.theme.ComposeTutorialsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeTutorialsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeTutorialsTheme {
+        Greeting("Android")
+    }
+}
