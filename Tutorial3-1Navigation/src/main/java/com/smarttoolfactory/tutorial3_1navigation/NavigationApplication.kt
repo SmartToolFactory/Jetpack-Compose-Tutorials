1a3db0119749e1140c729e0d5aca71794f348dbf
Smart Tool Factory <smarttoolfactory@icloud.com>
2024-10-17 13:29:05 +0300

    add NavigationApplication

---
+++ Tutorial3-1Navigation/src/main/java/com/smarttoolfactory/tutorial3_1navigation/NavigationApplication.kt
@@ -0,0 +1,7 @@
+package com.smarttoolfactory.tutorial3_1navigation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NavigationApplication : Application()
