210c3826610ae39d4901b56a393d6faeb276b2a8
Smart Tool Factory <smarttoolfactory@icloud.com>
2024-10-19 15:07:02 +0300

    update text in Tutorial2PopUpToAndLaunchSingleTop

--- Tutorial3-1Navigation/src/main/java/com/smarttoolfactory/tutorial3_1navigation/Tutorial2PopUpToAndLaunchSingleTop.kt
+++ Tutorial3-1Navigation/src/main/java/com/smarttoolfactory/tutorial3_1navigation/Tutorial2PopUpToAndLaunchSingleTop.kt
@@ -13,8 +13,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
@@ -27,7 +28,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -231,15 +234,28 @@ private fun RouteScreen(
         )
 
         val currentBackStack: List<NavBackStackEntry> by navController.currentBackStack.collectAsState()
+        val packageName = LocalContext.current.packageName
 
-        Column(
-            modifier = Modifier.verticalScroll(rememberScrollState())
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            currentBackStack.reversed().forEach {
+
+            // Don't do looped operations in actual code, it's for demonstration
+            items(items = currentBackStack.reversed()) {
                 Text(
-                    text = it.destination.route ?: it.destination.displayName,
-                    modifier = Modifier.fillMaxWidth(),
-                    fontSize = 18.sp
+                    text = it.destination.route
+                        ?.replace("$packageName.", "")
+                        ?.replace(
+                            "BottomNavigationRoute.",
+                            ""
+                        ) ?: it.destination.displayName,
+                    modifier = Modifier
+                        .shadow(4.dp, RoundedCornerShape(8.dp))
+                        .background(Color.White)
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    fontSize = 16.sp
                 )
             }
         }
